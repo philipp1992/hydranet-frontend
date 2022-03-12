365fa6267bca56730054b3ae864bd3fc520f3609
Raul Humberto Lopez Lopez <raul.hll@hotmail.com>
2022-03-11 18:44:04 -0700

    arbitrum mainnet deployment

--- src/App.tsx
+++ src/App.tsx
@@ -257,7 +257,7 @@ function App() {
       setWalletChecked(true);
     }
     if (shouldTriggerSafetyCheck()) {
-      dispatch(info("Safety Check: Always verify you're on app.olympusdao.finance!"));
+      dispatch(info("Safety Check: Always verify you're on https://hydranet.ai"));
     }
   }, []);
 
